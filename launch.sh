4800a0fcb59f934363dc33fbc5d08ac03d1678ba
devlaith <allame277@gmail.com>
2016-08-16 21:58:06 +0430

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -33,7 +33,17 @@ install_luarocks() {
 }
 
 install_rocks() {
-  ./.luarocks/bin/luarocks install luasocket
+  ./.luarocks/bin/luarocks install luasec
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+ ./.luarocks/bin/luarocks install lbase64 20120807-3
+ RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+  
+ ./.luarocks/bin/luarocks install luasocket
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -110,12 +120,6 @@ else
     echo "Run $0 install"
     exit 1
   fi
-
-  if [ -f $HOME/.telegram-cli/state ]; then
-    echo "state found"
-    echo "remove it!"
-    rm -rf $HOME/.telegram-cli/state
-  fi
-
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@ --disable-link-preview
+  rm -r ../.telegram-cli/state #Prevent tg from crash
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
 fi
